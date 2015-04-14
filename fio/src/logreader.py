7a8f6bf9c677ae0075cd6e18e19dcda9219567f1
Junjie Qian <qjj1983@gmail.com>
2015-04-14 09:23:08 -0500

    update the log index in fio results

--- fio/src/logreader.py
+++ fio/src/logreader.py
@@ -5,6 +5,11 @@ import os
 import sys
 
 class reader:
+  _workload = {}
+  _perf_path = ""
+  _iostat_path = ""
+  _workload_filename = ""
+
   def __init__(self, workload_filename, perf_path, iostat_path):
     self._perf_path = perf_path
     self._iostat_path = iostat_path
@@ -17,19 +22,19 @@ class reader:
         else:
           _testnum = row[0]
           _filesize = row[1]
-	  _blocksize = row[2]
-	  _patern = row[3]
-	  _access_list = _patern.split()
+          _blocksize = row[2]
+          _patern = row[3]
+          _access_list = _patern.split()
           _runtime = row[4]
-	  _ramptime = row[5]
-	  _startdelay = row[6]
-	  _workers = row[7]
-	  _iodepth = row[8]
-	  _device_num = len(row[9].split(":"))
-	  self._workload[_testnum] = row[1:2]
-	  self._workload[_testnum].append(_access_list)
-	  self._workload[_testnum].append(row[4:8])
-	  self._workload[_testnum].append(str(_device_num))
+          _ramptime = row[5]
+          _startdelay = row[6]
+          _workers = row[7]
+          _iodepth = row[8]
+          _device_num = len(row[9].split(":"))
+          self._workload[_testnum] = row[1:2]
+          self._workload[_testnum].append(_access_list)
+          self._workload[_testnum].append(row[4:8])
+          self._workload[_testnum].append(str(_device_num))
 
   def perf_read(self):
     if not os.path.isdir(self._perf_path):
@@ -39,11 +44,16 @@ class reader:
         fp = open(f)
 	for line in fp:
           word = line.split(";")
-	  _testnum = word[2]
-	  _throughput = word[]
-	  _disk_utilization = word[]
-	  _cpu_user = word[]
-	  _cpu_system = word[]
-          _context_switches = word[]
+          _testnum = word[2]
+	  _iops = word[7]
+          _throughput = word[6]
+          _disk_utilization = word[-1]
+          _cpu_user = word[87]
+          _cpu_system = word[88]
+          _context_switches = word[89]
           _bandwidth = word[]
-          _latency = word[]
+          _write_submission_latency = word[]
+	  _write_completion_latency = word[]
+	  _read_submission_latency  = word[]
+	  _read_completion_latency  = word[]
+        ret.writerow()

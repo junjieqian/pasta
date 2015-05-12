0e66be2af744153b1e1a1847eb06d9ec77fe8d2b
Junjie Qian <jqian.unl@gmail.com>
2015-05-12 10:48:59 -0500

    null workload used as baseline

---
+++ fio/src/reorg.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python
+
+import string
+import csv
+import sys
+
+def main():
+  _ret = sys.argv[1]
+  ret = csv.writer(open("fiolog_ret.csv", "wb"))
+  ret.writerow(["testnum", "iops", "disk utilization", "average bandwidth"])
+  strs = {}
+  with open(self._ret, 'rU') as csvreader:
+    reader = csv.reader(csvreader, delimiter=',', quotechar='|')
+    for row in reader:
+      if "testnum" in row:
+        continue
+      else:
+        x = int(row[0])%16
+        if not x in strs:
+          strs[x] = []
+        strs[x].append(row)
+  for x in strs:
+    for row in strs[x]:
+      ret.writerow(row)
+
+if __name__ == "__main__":
+  main()

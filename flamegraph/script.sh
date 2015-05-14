eaa2cb29fa0354a45ca1a747e30da59c3e4c3850
Junjie Qian <jqian.unl@gmail.com>
2015-05-14 10:25:19 -0500

    function call graph, flamegraph update

---
+++ flamegraph/script.sh
@@ -0,0 +1,11 @@
+#!/bin.bash
+
+# More about flamegraph, refer to http://www.brendangregg.com/FlameGraphs/cpuflamegraphs.html
+
+flamegraph=~/flamegraph/FlameGraph
+
+sudo ~/linux/tools/perf/perf record -F 99 -a -g -- fio --name=1 --bs=1m --ioengine=libaio --iodepth=32 --size=100g --rw=read --direct=1 --filename=/dev/nvme0n1 --numjobs=$1
+
+sudo ~/linux/tools/perf/perf script | $flamegraph/stackcollapse-perf.pl > out.perf-folded
+
+$flamegraph/flamegraph.pl out.perf-folded > perf-kernel.svg
